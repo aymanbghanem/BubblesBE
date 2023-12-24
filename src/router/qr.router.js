8965e92cb4220e352326e248f91d9957c4a029f1
Misk Sawalha <Misk.Sawalha@Jawwal.ps>
2023-12-24 12:18:40 +0200

    qr

--- src/router/qr.router.js
+++ src/router/qr.router.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const auth = require("../middleware/auth");
+const locationModels = require("../models/location.models");
+const surveyModels = require("../models/survey.models");
+const { create } = require("../models/user.models");const qrModel = require("../models/qr.model");
 const router = express.Router();
 
 
@@ -11,7 +14,19 @@ router.post('/api/v1/addQR',auth,async(req,res)=>{
         let survey_id = req.headers['survey_id']
         let {link} = req.body
         if(role=="admin"){
-          
+           let existingLocation = await locationModels.findOne({_id:location_id,active:1})
+           let existingSurvey = await surveyModels.findOne({_id:survey_id,active:1})
+           if(existingLocation && existingSurvey ){
+              let qr = await qrModel.create({
+                location_id : location_id,
+                survey_id : survey_id,
+                link:link
+              })
+              res.json({message:"The QR code link successfully added",qr})
+           }
+           else{
+            res.json({message:"The survey or the location does not exist"})
+           }
         }
         else{
             res.json({message:"sorry, you are unauthorized"})
