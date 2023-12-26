cbb53d0438eea0f9b52d46377bdcead4ac176883
Misk Sawalha <Misk.Sawalha@Jawwal.ps>
2023-12-26 11:34:17 +0200

    questions

--- src/router/survey.router.js
+++ src/router/survey.router.js
@@ -485,145 +485,80 @@ router.put('/api/v1/updateSurvey', auth, async (req, res) => {
 });
 
 
-router.post('/api/v1/getInitialQuestions', async (req, res) => {
-  try {
-    const { survey_id, phase, answers } = req.body;
+// router.post('/api/v1/getInitialQuestions', async (req, res) => {
+//   try {
+//     const { survey_id, phase, answers } = req.body;
 
-    // Validate survey existence and active status
-    const existingSurvey = await surveyModel.findOne({
-      _id: survey_id,
-      active: 1,
-    });
+//     // Validate survey existence and active status
+//     const existingSurvey = await surveyModel.findOne({
+//       _id: survey_id,
+//       active: 1,
+//     });
 
-    if (!existingSurvey) {
-      return res.status(404).json({ message: "The survey does not exist or is not active." });
-    }
+//     if (!existingSurvey) {
+//       return res.status(404).json({ message: "The survey does not exist or is not active." });
+//     }
 
-    // Fetch questions for the first phase
-    const firstPhaseQuestions = await Question.find({
-      survey_id: survey_id,
-      active: 1,
-      phase: 1,
-    })
-      .populate({
-        path: 'answers',
-        model: 'answer',
-        select: 'answer image',
-      })
-      .populate({
-        path: 'question_type',
-        model: 'question_controller',
-        select: 'question_type',
-      })
-      .select('question_title answers question_type');
+//     // Fetch questions for the first phase
+//     const firstPhaseQuestions = await Question.find({
+//       survey_id: survey_id,
+//       active: 1,
+//       phase: 1,
+//     })
+//       .populate({
+//         path: 'answers',
+//         model: 'answer',
+//         select: 'answer image',
+//       })
+//       .populate({
+//         path: 'question_type',
+//         model: 'question_controller',
+//         select: 'question_type',
+//       })
+//       .select('question_title answers question_type required');
 
-    if (!firstPhaseQuestions || firstPhaseQuestions.length === 0) {
-      return res.status(404).json({ message: "No questions found for the first phase." });
-    }
+//     if (!firstPhaseQuestions || firstPhaseQuestions.length === 0) {
+//       return res.status(404).json({ message: "No questions found for the first phase." });
+//     }
 
-    // Flatten the question_type field
-    const flattenedQuestions = firstPhaseQuestions.map(question => {
-      return {
-        _id: question._id,
-        question_title: question.question_title,
-        answers: question.answers,
-        question_type: question.question_type.question_type,
-      };
-    });
+//     // Flatten the question_type field
+//     const flattenedQuestions = firstPhaseQuestions.map(question => {
+//       return {
+//         _id: question._id,
+//         question_title: question.question_title,
+//         answers: question.answers,
+//         question_type: question.question_type.question_type,
+//       };
+//     });
 
-    res.json({ questions: flattenedQuestions });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+//     res.json({ questions: flattenedQuestions });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
 
 
 //get questions from the specific phase
 
-// router.post('/api/v1/getQuestions', async (req, res) => {
-//   try {
-//     const results = [];
-//     let survey_id = req.headers['survey_id']
-//     const { phase, answered_questions } = req.body;
-     
-//     let phaseQuestions = await Question.find({ survey_id: survey_id, phase: phase, active: 1 })
-//     .populate({
-//       path: 'question_type',
-//       model: 'question_controller',
-//       select: 'question_type',
-//     });
-
-//     const responses = [];
-//     const maxPhase = await Question.findOne({ survey_id: survey_id, active: 1 })
-//       .sort({ phase: -1 }) // Sort in descending order of phase number
-//       .limit(1)
-//       .select('phase');
-      
-//     let phaseNum = maxPhase.phase
-    
-//     if (phase <= phaseNum) {
-//       for (const question of phaseQuestions) {
-//         let dependenciesSatisfied = true;
-
-//         if (question.question_dependency && question.question_dependency.length > 0) {
-//           if (question.question_dependency.length === 1) {
-//             // Keep the existing logic for a single dependency
-//             const dependency = question.question_dependency[0];
-//             const isDependencySatisfied = await checkDependencySatisfaction(dependency, answered_questions, results);
-
-//             if (!isDependencySatisfied) {
-//               dependenciesSatisfied = false;
-//             }
-//           } else {
-//             // New logic for multiple dependencies
-//             const isMultipleDependenciesSatisfied = await checkMultipleDependenciesSatisfaction(question.question_dependency, answered_questions, results);
-
-//             if (!isMultipleDependenciesSatisfied) {
-//               dependenciesSatisfied = false;
-//             }
-//           }
-//         }
-
-//         if (dependenciesSatisfied) {
-//           responses.push({
-//             _id: question._id,
-//             question_text: question.question_title,
-//             phase: question.phase,
-//             question_type: question.question_type ? question.question_type.question_type : null,
-//           });
-//         }
-//       }
-//       return res.json(responses);
-//     } else {
-//       res.json({ message: "No more questions" });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// });
-
 router.post('/api/v1/getQuestions', async (req, res) => {
   try {
+    const results = [];
+    let survey_id = req.headers['survey_id']
     const { phase, answered_questions } = req.body;
-    let survey_id = req.headers['survey_id'];
-
-    // Validate survey existence and active status
-    const existingSurvey = await surveyModel.findOne({
-      _id: survey_id,
-      active: 1,
-    });
-
-    if (!existingSurvey) {
-      return res.status(404).json({ message: "The survey does not exist or is not active." });
-    }
-
-    let questions;
-    
-    if (phase === 1) {
+     
+    if(phase==1){
+      const existingSurvey = await surveyModel.findOne({
+        _id: survey_id,
+        active: 1,
+      });
+  
+      if (!existingSurvey) {
+        return res.status(404).json({ message: "The survey does not exist or is not active." });
+      }
+  
       // Fetch questions for the first phase
-      questions = await Question.find({
+      const firstPhaseQuestions = await Question.find({
         survey_id: survey_id,
         active: 1,
         phase: 1,
@@ -638,11 +573,27 @@ router.post('/api/v1/getQuestions', async (req, res) => {
           model: 'question_controller',
           select: 'question_type',
         })
-        .select('_id question_title answers question_type');
-    } else {
-      // Fetch questions for the specific phase, including answers
-      questions = await Question.find({ survey_id: survey_id, phase: phase, active: 1 })
-        .populate({
+        .select('question_title answers question_type required');
+  
+      if (!firstPhaseQuestions || firstPhaseQuestions.length === 0) {
+        return res.status(404).json({ message: "No questions found for the first phase." });
+      }
+  
+      // Flatten the question_type field
+      const flattenedQuestions = firstPhaseQuestions.map(question => {
+        return {
+          _id: question._id,
+          question_title: question.question_title,
+          answers: question.answers,
+          question_type: question.question_type.question_type,
+        };
+      });
+  
+      res.json({ questions: flattenedQuestions });
+    }
+    else{
+      let phaseQuestions = await Question.find({ survey_id: survey_id, phase: phase, active: 1 })
+     .populate({
           path: 'answers',
           model: 'answer',
           select: 'answer image',
@@ -652,57 +603,63 @@ router.post('/api/v1/getQuestions', async (req, res) => {
           model: 'question_controller',
           select: 'question_type',
         })
-        .select('_id question_title answers question_type');
-    }
-
-    const responses = questions
-      .filter(question => {
-        let dependenciesSatisfied = true;
-
-        if (question.question_dependency && question.question_dependency.length > 0) {
-          if (question.question_dependency.length === 1) {
-            const dependency = question.question_dependency[0];
-            const isDependencySatisfied = checkDependencySatisfaction(dependency, answered_questions, []);
-
-            if (!isDependencySatisfied) {
-              dependenciesSatisfied = false;
-            }
-          } else {
-            const isMultipleDependenciesSatisfied = checkMultipleDependenciesSatisfaction(question.question_dependency, answered_questions, []);
-
-            if (!isMultipleDependenciesSatisfied) {
-              dependenciesSatisfied = false;
+      const responses = [];
+      const maxPhase = await Question.findOne({ survey_id: survey_id, active: 1 })
+        .sort({ phase: -1 }) // Sort in descending order of phase number
+        .limit(1)
+        .select('phase');
+        
+      let phaseNum = maxPhase.phase
+      
+      if (phase <= phaseNum) {
+        for (const question of phaseQuestions) {
+          let dependenciesSatisfied = true;
+  
+          if (question.question_dependency && question.question_dependency.length > 0) {
+            if (question.question_dependency.length === 1) {
+              // Keep the existing logic for a single dependency
+              const dependency = question.question_dependency[0];
+              const isDependencySatisfied = await checkDependencySatisfaction(dependency, answered_questions, results);
+  
+              if (!isDependencySatisfied) {
+                dependenciesSatisfied = false;
+              }
+            } else {
+              // New logic for multiple dependencies
+              const isMultipleDependenciesSatisfied = await checkMultipleDependenciesSatisfaction(question.question_dependency, answered_questions, results);
+  
+              if (!isMultipleDependenciesSatisfied) {
+                dependenciesSatisfied = false;
+              }
             }
           }
+  
+          if (dependenciesSatisfied) {
+            responses.push({
+              _id: question._id,
+              question_text: question.question_title,
+              phase: question.phase,
+              question_type: question.question_type ? question.question_type.question_type : null,
+              answers: question.answers.map(answer => ({
+                _id: answer._id,
+                answer: answer.answer,
+                image: answer.image,
+              })),
+            });
+          }
         }
-
-        return dependenciesSatisfied;
-      })
-      .map(question => {
-        return {
-          _id: question._id,
-          question_text: question.question_title,
-          phase: phase, // Include the phase in the response
-          question_type: question.question_type ? question.question_type.question_type : null,
-          answers: question.answers ? question.answers.map(answer => ({
-            _id: answer._id,
-            answer: answer.answer,
-            image: answer.image,
-          })) : [],
-        };
-      });
-
-    if (responses.length > 0) {
-      return res.json(responses);
-    } else {
-      return res.json({ message: "No more questions" });
+        return res.json(responses);
+      } 
+      else {
+        res.json({ message: "No more questions" });
+      }
     }
+  
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
 });
-
 
 async function checkDependencySatisfaction(dependency, answeredQuestions, results) {
   const parentQuestionId = dependency.parent_id.toString();
@@ -789,8 +746,6 @@ async function checkMultipleDependenciesSatisfaction(dependencies, answeredQuest
 
   return overallSatisfied;
 }
-
-
 async function checkRangeDependency(dependency, answeredQuestions, results) {
   // Implement the logic for "Range" type dependencies
   // You can use a similar approach as in checkDependencySatisfaction for "Range" type
@@ -817,6 +772,7 @@ async function checkRangeDependency(dependency, answeredQuestions, results) {
 
   return false;
 }
+
 
 router.delete('/api/v1/deleteSurvey', auth, async (req, res) => {
   try {
