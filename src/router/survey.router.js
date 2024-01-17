9263e3c7f0625a8d3ad9aef902ba64bdf20c1183
Misk Sawalha <Misk.Sawalha@Jawwal.ps>
2024-01-17 14:42:24 +0200

    symbol_size

--- src/router/survey.router.js
+++ src/router/survey.router.js
@@ -393,6 +393,7 @@ router.put('/api/v1/updateSurvey', auth, async (req, res) => {
               title_font_size: updatedSurveyData.title_font_size || existingSurvey.title_font_size,
               description_font_size: updatedSurveyData.description_font_size || existingSurvey.description_font_size,
               response_message: updatedSurveyData.response_message || existingSurvey.response_message,
+              symbol_size : updatedSurveyData.symbol_size || existingSurvey.symbol_size
             },
           }
         );
