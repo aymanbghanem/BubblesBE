cfa6a5d6c9b52fd62169cf81fab427898e4cdc3c
Misk Sawalha <Misk.Sawalha@Jawwal.ps>
2024-04-02 12:45:56 +0200

    password generator

--- src/helper/passwordGenerator.helper.js
+++ src/helper/passwordGenerator.helper.js
@@ -5,12 +5,22 @@ function generateMixedID() {
   var uuidValue = uuid.v4();
 
   // Extract the alphanumeric part from the UUID
-  var alphanumericPart = uuidValue.replace(/-/g, '').substring(0, 6);
+  var alphanumericPart = uuidValue.replace(/-/g, '');
 
-  return alphanumericPart;
+  // Add special characters to the alphanumeric part
+  var specialCharacters = "!@#$&*";
+  var mixedID = '';
+  for (var i = 0; i < alphanumericPart.length; i++) {
+    mixedID += alphanumericPart[i];
+    // Randomly decide whether to add a special character or not
+    if (Math.random() < 0.25 && mixedID.length < 10) {
+      mixedID += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    }
+  }
+
+  // Ensure the length of the mixedID is at most 10 characters
+  return mixedID.substring(0, 10);
 }
-
-
 
 // Export the model's ID
 module.exports = generateMixedID;
